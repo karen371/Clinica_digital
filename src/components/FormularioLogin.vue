24f33ffeb80c05dde19aa295c6f1930eeef9c875
karen371 <karengodoy325@gmail.com>
2024-11-28 18:53:48 -0300

    pagina principal login

---
+++ src/components/FormularioLogin.vue
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+<template>
+  <form class="formulario">
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Correo Electrónico</label>
+      <input 
+        type="email" 
+        class="form-control" 
+        id="exampleInputEmail1" 
+        aria-describedby="emailHelp"
+      >
+      <div id="emailHelp" class="form-text">
+        Nunca compartiremos tu correo electrónico con nadie más.
+      </div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+      <input 
+        type="password" 
+        class="form-control" 
+        id="exampleInputPassword1"
+      >
+    </div>
+    <div class="mb-3 form-check">
+      <input 
+        type="checkbox" 
+        class="form-check-input" 
+        id="exampleCheck1"
+      >
+      <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+  </form>
+</template>
+<style lang="sass" scoped>
+.formulario
+  color: var(--vt-c-blue)
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<script setup>
+import login from '../components/FormularioLogin.vue'
+</script>
+
+<template>
+  <section class="container contenedor__login">
+    <div class="d-flex flex-column justify-content-center align-items-center min-vh-100">
+      <!-- Título y texto en la parte superior -->
+      <div class="text-center mt-5 contenedor__login-titulo">
+        <h1>Clinica Digital</h1>
+        <p>Inicie sesión para gestionar sus citas</p>
+      </div>
+      <!-- Componente login en la parte inferior -->
+      <div class="mb-5 pt-3">
+        <login/>
+      </div>
+    </div>    
+  </section>  
+</template>
+<style lang="sass" scoped>
+.contenedor__login
+  // Estilo general del contenedor
+  background-color: #f0f4f8
+  min-height: 100vh
+  display: flex
+  flex-direction: column
+  justify-content: space-between
+  padding: 20px
+
+  &-titulo
+    // Estilos para el título (h1) y párrafo (p)
+    text-align: center
+
+    h1
+      color: var(--vt-c-divider-light-1)
+      font-size: 2.5rem
+      font-weight: bold
+      margin-bottom: 10px
+
+    p
+      color: #333
+      font-size: 1rem
+      margin: 0
+</style>
+
