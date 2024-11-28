a389bd67da11cf58ee485f2b5edf4c7fe31626ab
karen371 <karengodoy325@gmail.com>
2024-11-28 19:40:40 -0300

    cambios en pagina dashboard

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,26 +1,27 @@
 <script></script>
 <template>
     <table class="table">
-        <thead>
-        <tr>
+        <thead class="table-primary">
+          <tr>
             <th scope="col">Fecha</th>
             <th scope="col">Hora</th>
             <th scope="col">Especialidad</th>
             <th scope="col">Medico</th>
             <th scope="col">Estado</th>
-        </tr>
+          </tr>
         </thead>
         <tbody>
-        <tr>
+          <tr>
             <td>30/11/2024</td>
             <td>02:00 PM</td>
             <td>Dermatologia</td>
             <td>Dr. Morales</td>
             <td>Pendiente</td>
-        </tr>
+          </tr>
         </tbody>
-    </table>
+      </table> 
 </template>
 <style lang="sass" scoped>
-
+    
+    
 </style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -7,12 +7,13 @@ import Navbar from '@/components/Navbar.vue';
   <Navbar/>
   <section class="contenedor-bienvenida my-5">
     <!-- Sección de bienvenida -->
-    <div class="contenedor-bienvenida__cabecera mb-4">
+    <div class="contenedor-bienvenida__cabecera mb-4 text-center">
       <h2 class="contenedor-bienvenida__titulo">¡Bienvenido, Pedro Pérez!</h2>
       <p class="contenedor-bienvenida__mensaje text-muted">Recuerde que su próxima cita es el bal bla</p>
     </div>
     <!-- Tabla de citas o información -->
     <div class="contenedor-bienvenida__tabla">
+      <h3>Estados de citas agendadas</h3>
       <Table/>
     </div>
   </section>
@@ -22,20 +23,25 @@ import Navbar from '@/components/Navbar.vue';
 /* Estilos personalizados usando BEM */
 .contenedor-bienvenida
   background-color: #f9f9f9
-  padding: 30px
   border-radius: 8px
   box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1)
 
 .contenedor-bienvenida__cabecera
   margin-bottom: 1.5rem
-
+  background-color: var(--color-background-soft)
+  height: 100px
+  border-radius: 10px
+  border: 1px solid var(--color-border-hover)
+  
 .contenedor-bienvenida__titulo
   font-weight: bold
   color: var(--color-heading)
+  margin-left: 30px 
 
 .contenedor-bienvenida__mensaje
   font-size: 1rem
   color: var(--color-text)
+  margin-left: 30px 
 
 .contenedor-bienvenida__tabla
   margin-top: 20px
